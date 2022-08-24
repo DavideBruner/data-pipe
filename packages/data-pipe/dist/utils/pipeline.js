420e1efcb43790a26be6b8c4d65519327c18d69e
Davide Bruner <davidebruner@hotmail.it>
2022-08-25 01:46:38 +0200

    chore(release): publish

--- packages/data-pipe/dist/utils/pipeline.js
+++ packages/data-pipe/dist/utils/pipeline.js
@@ -1,3 +1,3 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.default = (fns, ...options) => fns.reduce((v, f) => f(v, Object.assign(Object.assign({}, options), f === null || f === void 0 ? void 0 : f.options)), []);
+exports.default = (fns, ...options) => fns.reduce((v, f) => f(v, ...options), []);
