3ac4abde7c55d99cd16a000b1f0b7f165c8f96f6
Davide Bruner <davidebruner@hotmail.it>
2022-08-25 03:17:48 +0200

    chore(release): publish

--- packages/data-pipe/src/lazyReadData.ts
+++ packages/data-pipe/src/lazyReadData.ts
@@ -4,11 +4,12 @@ import readData from './readData';
 export default function lazyReadData({ processors, ...config }: ConfigData, options = {}) {
   let data: any = { current: null };
   let errors: any[] = [];
-  return [{ data, errors }, (opts: any) => {
+  const read = (opts: any) => {
     try {
       data.current = readData({ processors, ...config }, { ...options, ...opts });
     } catch(e: any) {
       errors.push(e.message);
     }
-  }];
+  }
+  return [{ data, errors }, read];
 }
